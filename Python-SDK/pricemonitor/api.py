36f334cddd98a6d7ce5bdd57c6763bc6cd055871
Gabriel Andrade <gabiruh@gmail.com>
2018-08-26 20:51:26 -0300

    python api update

--- Python-SDK/pricemonitor/api.py
+++ Python-SDK/pricemonitor/api.py
@@ -30,6 +30,27 @@ class Api():
         test=bool,
         pformat_dec=Validator.price_format)
     def get_price_updates(self, **kwargs):
+        """Perform GET request to get price updates.
+
+        Parameters
+        ----------
+        marketplace : str
+            Marketplace name. ex: `idealo.de`, `google.de`
+        format : { 'json', 'csv'} , optional
+            Output file format
+        exportall: bool, optional
+            Get all products or product with a price update
+        id : int, optional
+            Specifying product ID
+        test: bool, optional
+            Get random prices
+        pformat_dec: { 1, 2 }, optional
+            Price format, integer (1) or decimal (2)
+        
+        Returns
+        -------
+        requests.models.Response
+        """
         assert kwargs.get('marketplace') is not None
 
         return requests.get(
@@ -45,6 +66,29 @@ class Api():
         cleanold=bool,
         test=bool)
     def import_products(self, **kwargs):
+        """Perform POST request to update products.
+
+        Parameters
+        ----------
+        marketplace : str
+            Marketplace name. ex: `idealo.de`, `google.de`
+        file : str
+            Path for upload file, must be readable
+        separator : { 'comma', 'semicolon', 'tab' }, optional
+            Get all products or product with a price update
+        lineend : { 'win', 'unix'} , optional
+            Line end format, windows \r\n (win) or unix \n (unix)
+        keepold: bool, optional
+            Deletes old products from database
+        cleanold: bool, optional
+            Deletes products not included in csv file from database
+        test: bool, optional
+            Data won't be saved to database
+        
+        Returns
+        -------
+        requests.models.Response
+        """        
         assert kwargs.get('marketplace') is not None
         assert kwargs.get('file') is not None
         with open(kwargs.get('file'), 'rb') as fh:
@@ -65,6 +109,30 @@ class Api():
         ids=Validator.ids,
         pformat_dec=Validator.price_format)
     def export(self, **kwargs):
+        """Perform GET request to get product offers.
+
+        Parameters
+        ----------
+        marketplace : str
+            Marketplace name. ex: `idealo.de`, `google.de`
+        format : { 'json', 'csv'} , optional
+            Output file format
+        exportall: bool, optional
+            Get all products or product with a price update
+        sortby:  { 'total_price', 'price', 'shipping_costs', 'ranking'}, optional
+            Sort by specified fields
+        offeridx : int, optional
+            Specifying offer ID
+        ids: str or array, optional
+            Comma separated list if ids or array of ids 
+        pformat_dec: { 1, 2 }, optional
+            Price format, integer (1) or decimal (2)
+        
+        Returns
+        -------
+        requests.models.Response
+        """        
+        
         assert kwargs.get('marketplace') is not None
         ids = kwargs.get('ids')
         if ids is not None and isinstance(ids, list):
@@ -75,12 +143,40 @@ class Api():
 
     @accepts(object, marketplace=str, format=Validator.file_format)
     def get_errors(self, **kwargs):
+        """Perform GET request to get products with an error status.
+
+        Parameters
+        ----------
+        marketplace : str
+            Marketplace name. ex: `idealo.de`, `google.de`
+        format : {'json', 'csv'}, optional
+            Output file format
+        
+        Returns
+        -------
+        requests.models.Response
+        """        
+
         assert kwargs.get('marketplace') is not None
         return requests.get(
             self._uri_for('get_errors'), params=kwargs, verify=False)
 
     @accepts(object, marketplace=str, ids=Validator.ids,)
     def delete_products(self, **kwargs):
+        """Perform GET request to delete products.
+
+        Parameters
+        ----------
+        marketplace : str
+            Marketplace name. ex: `idealo.de`, `google.de`
+        ids: str or array
+            String with comma separated list if ids or array of ids 
+        
+        Returns
+        -------
+        requests.models.Response
+        """        
+
         assert kwargs.get('marketplace') is not None
         assert kwargs.get('ids') is not None
         ids = kwargs.get('ids')
@@ -95,8 +191,26 @@ class Api():
         marketplace=str,
         url=str,
         repricing=Validator.repricing,
-        ean=str)
+        ean=Validator.ean)
     def marketplace_settings(self, **kwargs):
+        """Perform GET request to delete products.
+
+        Parameters
+        ----------
+        marketplace : str
+            Marketplace name. ex: `idealo.de`, `google.de`
+        url: str
+            Sets default URL for marketplace
+        repricing: {'on', 'off'}
+            Enable/Disable repricing. Default `on`
+        ean: {'on', 'off'}
+            Default is `on`
+        
+        Returns
+        -------
+        requests.models.Response
+        """        
+        
         assert kwargs.get('marketplace') is not None
         assert kwargs.get('url') is not None
         assert kwargs.get('repricing') is not None
@@ -111,11 +225,28 @@ class Api():
         ids=Validator.ids,
         pformat_dec=Validator.price_format)
     def get_reprice_settings(self, **kwargs):
+        """Perform GET request to get reprice settings.
+
+        Parameters
+        ----------
+        marketplace : str
+            Marketplace name. ex: `idealo.de`, `google.de`
+        format : {'json', 'csv'}, optional
+            Output file format
+        ids: str or array
+            String with comma separated list if ids or array of ids 
+        pformat_dec: { 1, 2 }, optional
+            Price format, integer (1) or decimal (2)
+        
+        
+        Returns
+        -------
+        requests.models.Response
+        """        
         assert kwargs.get('marketplace') is not None
         ids = kwargs.get('ids')
         if ids is not None and isinstance(ids, list):
             kwargs['ids'] =  ','.join(ids)
-
         
         return requests.get(
             self._uri_for('reprice_settings'), params=kwargs, verify=False)
@@ -123,9 +254,30 @@ class Api():
     @accepts(
         object,
         marketplace=str,
+        file=Validator.upload,
         separator=Validator.separator,
         lineend=Validator.line_end)
     def set_reprice_settings(self, **kwargs):
+        """Perform POST request to set reprice settings.
+
+        Parameters
+        ----------
+        marketplace : str
+            Marketplace name. ex: `idealo.de`, `google.de`
+        file : str
+            Path for CSV filename containing reprice settings
+        separator : { 'comma', 'semicolon', 'tab' }, optional
+            Get all products or product with a price update
+        lineend : { 'win', 'unix'} , optional
+            Line end format, windows \r\n (win) or unix \n (unix)
+        
+        
+        Returns
+        -------
+        requests.models.Response
+        """        
+
         assert kwargs.get('marketplace') is not None
+        assert kwargs.get('file') is not None        
         return requests.post(
             self._uri_for('reprice_settings'), params=kwargs, verify=False)
